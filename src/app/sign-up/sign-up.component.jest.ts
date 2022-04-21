ab0f054974db867d6be58328c40a68b1d72edd8f
Basar Buyukkahraman <basarbk@yahoo.com>
2022-04-21 22:34:29 +0300

    0302-layout-jest

--- src/app/sign-up/sign-up.component.jest.ts
+++ src/app/sign-up/sign-up.component.jest.ts
@@ -1,8 +1,53 @@
-import { render, screen } from "@testing-library/angular";
-import { SignUpComponent } from "./sign-up.component"
+import { render, screen } from '@testing-library/angular';
+import { SignUpComponent } from './sign-up.component';
 
-it('has Sign Up header', async () => {
-  await render(SignUpComponent);
-  const header = screen.getByRole('heading', { name: 'Sign Up'});
-  expect(header).toBeInTheDocument();
+describe('SignUpComponent', () => {
+  describe('Layout', () => {
+    it('has Sign Up header', async () => {
+      await render(SignUpComponent);
+      const header = screen.getByRole('heading', { name: 'Sign Up' });
+      expect(header).toBeInTheDocument();
+    });
+    it('has username input', async () => {
+      await render(SignUpComponent);
+      expect(screen.getByLabelText('Username')).toBeInTheDocument();
+    });
+
+    it('has email input', async () => {
+      await render(SignUpComponent);
+      expect(screen.getByLabelText('E-mail')).toBeInTheDocument();
+    });
+
+    it('has password input', async () => {
+      await render(SignUpComponent);
+      expect(screen.getByLabelText('Password')).toBeInTheDocument();
+    });
+
+    it('has password type for password input', async () => {
+      await render(SignUpComponent);
+      const input = screen.getByLabelText('Password');
+      expect(input).toHaveAttribute('type', 'password');
+    });
+
+    it('has password repeat input', async () => {
+      await render(SignUpComponent);
+      expect(screen.getByLabelText('Password Repeat')).toBeInTheDocument();
+    });
+
+    it('has password type for password repeat input', async () => {
+      await render(SignUpComponent);
+      const input = screen.getByLabelText('Password Repeat');
+      expect(input).toHaveAttribute('type', 'password');
+    });
+    it('has Sign Up button', async () => {
+      await render(SignUpComponent);
+      const button = screen.getByRole('button', { name: 'Sign Up' });
+      expect(button).toBeInTheDocument();
+    });
+    it('disables the button initially', async () => {
+      await render(SignUpComponent);
+      const button = screen.getByRole('button', { name: 'Sign Up' });
+      expect(button).toBeDisabled();
+    });
+  });
 });
