3778a6ef79973dfa08721f474f8dfaf21f217e44
Basar Buyukkahraman <basarbk@yahoo.com>
2022-04-22 22:20:27 +0300

    0303-interactions-jest

--- src/app/sign-up/sign-up.component.jest.ts
+++ src/app/sign-up/sign-up.component.jest.ts
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/angular';
 import { SignUpComponent } from './sign-up.component';
+import userEvent from "@testing-library/user-event";
 
 describe('SignUpComponent', () => {
   describe('Layout', () => {
@@ -49,5 +50,16 @@ describe('SignUpComponent', () => {
       const button = screen.getByRole('button', { name: 'Sign Up' });
       expect(button).toBeDisabled();
     });
+  });
+  describe('Interactions', () => {
+    it('enabels the button when the password and password repeat fields have same value', async () => {
+      await render(SignUpComponent);
+      const password = screen.getByLabelText('Password');
+      const passwordRepeat = screen.getByLabelText('Password Repeat');
+      await userEvent.type(password, "P4ssword");
+      await userEvent.type(passwordRepeat, "P4ssword");
+      const button = screen.getByRole('button', { name: 'Sign Up' });
+      expect(button).toBeEnabled();
+    })
   });
 });
