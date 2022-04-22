2a56c40635d5dbeb345d8148152450ee9c2e23a4
Basar Buyukkahraman <basarbk@yahoo.com>
2022-04-22 23:16:51 +0300

    0306-http-client-jest

--- src/app/sign-up/sign-up.component.jest.ts
+++ src/app/sign-up/sign-up.component.jest.ts
@@ -2,59 +2,67 @@ import { render, screen } from '@testing-library/angular';
 import { SignUpComponent } from './sign-up.component';
 import userEvent from "@testing-library/user-event";
 import "whatwg-fetch";
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+const setup = async () => {
+  await render(SignUpComponent, {
+    imports: [HttpClientTestingModule]
+  });
+}
 
 describe('SignUpComponent', () => {
   describe('Layout', () => {
     it('has Sign Up header', async () => {
-      await render(SignUpComponent);
+      await setup();
       const header = screen.getByRole('heading', { name: 'Sign Up' });
       expect(header).toBeInTheDocument();
     });
     it('has username input', async () => {
-      await render(SignUpComponent);
+      await setup();
       expect(screen.getByLabelText('Username')).toBeInTheDocument();
     });
 
     it('has email input', async () => {
-      await render(SignUpComponent);
+      await setup();
       expect(screen.getByLabelText('E-mail')).toBeInTheDocument();
     });
 
     it('has password input', async () => {
-      await render(SignUpComponent);
+      await setup();
       expect(screen.getByLabelText('Password')).toBeInTheDocument();
     });
 
     it('has password type for password input', async () => {
-      await render(SignUpComponent);
+      await setup();
       const input = screen.getByLabelText('Password');
       expect(input).toHaveAttribute('type', 'password');
     });
 
     it('has password repeat input', async () => {
-      await render(SignUpComponent);
+      await setup();
       expect(screen.getByLabelText('Password Repeat')).toBeInTheDocument();
     });
 
     it('has password type for password repeat input', async () => {
-      await render(SignUpComponent);
+      await setup();
       const input = screen.getByLabelText('Password Repeat');
       expect(input).toHaveAttribute('type', 'password');
     });
     it('has Sign Up button', async () => {
-      await render(SignUpComponent);
+      await setup();
       const button = screen.getByRole('button', { name: 'Sign Up' });
       expect(button).toBeInTheDocument();
     });
     it('disables the button initially', async () => {
-      await render(SignUpComponent);
+      await setup();
       const button = screen.getByRole('button', { name: 'Sign Up' });
       expect(button).toBeDisabled();
     });
   });
   describe('Interactions', () => {
     it('enables the button when the password and password repeat fields have same value', async () => {
-      await render(SignUpComponent);
+      await setup();
       const password = screen.getByLabelText('Password');
       const passwordRepeat = screen.getByLabelText('Password Repeat');
       await userEvent.type(password, "P4ssword");
@@ -63,8 +71,8 @@ describe('SignUpComponent', () => {
       expect(button).toBeEnabled();
     })
     it('sends username, email and password to backend after clicking the button', async () => {
-      const spy = jest.spyOn(window, 'fetch');
-      await render(SignUpComponent);
+      await setup();
+      let httpTestingController = TestBed.inject(HttpTestingController);
       const username = screen.getByLabelText('Username');
       const email = screen.getByLabelText('E-mail');
       const password = screen.getByLabelText('Password');
@@ -75,13 +83,13 @@ describe('SignUpComponent', () => {
       await userEvent.type(passwordRepeat, "P4ssword");
       const button = screen.getByRole('button', { name: 'Sign Up' });
       await userEvent.click(button);
-      const args = spy.mock.calls[0]
-      const secondParam = args[1] as RequestInit;
-      expect(secondParam.body).toEqual(JSON.stringify({
+      const req = httpTestingController.expectOne("/api/1.0/users");
+      const requestBody = req.request.body;
+      expect(requestBody).toEqual({
         username: "user1",
         password: "P4ssword",
         email: "user1@mail.com"
-      }))
+      })
       
     })
   });
