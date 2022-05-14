2c46b8823d1fa1f5ca3b237a10efc0c131797a03
Basar Buyukkahraman <basarbk@yahoo.com>
2022-05-14 16:50:25 +0300

    0504-dynamic-tests

--- src/app/sign-up/sign-up.component.jest.ts
+++ src/app/sign-up/sign-up.component.jest.ts
@@ -1,8 +1,8 @@
 import { render, screen, waitFor } from '@testing-library/angular';
 import { SignUpComponent } from './sign-up.component';
-import userEvent from "@testing-library/user-event";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
+import userEvent from '@testing-library/user-event';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import { HttpClientModule } from '@angular/common/http';
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -11,26 +11,25 @@ let requestBody: any;
 let counter = 0;
 const server = setupServer(
   rest.post('/api/1.0/users', (req, res, ctx) => {
-    requestBody = req.body
+    requestBody = req.body;
     counter += 1;
-    return res(ctx.status(200), ctx.json({}))
+    return res(ctx.status(200), ctx.json({}));
   })
 );
 
 beforeEach(() => {
   counter = 0;
-})
+});
 
-beforeAll(() => server.listen() )
+beforeAll(() => server.listen());
 
-afterAll(() => server.close())
-
+afterAll(() => server.close());
 
 const setup = async () => {
   await render(SignUpComponent, {
     imports: [HttpClientModule, SharedModule, ReactiveFormsModule],
   });
-}
+};
 
 describe('SignUpComponent', () => {
   describe('Layout', () => {
@@ -89,75 +88,78 @@ describe('SignUpComponent', () => {
       const email = screen.getByLabelText('E-mail');
       const password = screen.getByLabelText('Password');
       const passwordRepeat = screen.getByLabelText('Password Repeat');
-      await userEvent.type(username, "user1");
-      await userEvent.type(email, "user1@mail.com");
-      await userEvent.type(password, "P4ssword");
-      await userEvent.type(passwordRepeat, "P4ssword");
+      await userEvent.type(username, 'user1');
+      await userEvent.type(email, 'user1@mail.com');
+      await userEvent.type(password, 'P4ssword');
+      await userEvent.type(passwordRepeat, 'P4ssword');
       button = screen.getByRole('button', { name: 'Sign Up' });
-    }
+    };
     it('enables the button when the password and password repeat fields have same value', async () => {
       await setupForm();
       expect(button).toBeEnabled();
-    })
+    });
     it('sends username, email and password to backend after clicking the button', async () => {
       await setupForm();
       await userEvent.click(button);
 
       await waitFor(() => {
         expect(requestBody).toEqual({
-          username: "user1",
-          password: "P4ssword",
-          email: "user1@mail.com"
-        })
-      })
-    })
+          username: 'user1',
+          password: 'P4ssword',
+          email: 'user1@mail.com',
+        });
+      });
+    });
     it('disables button when there is an ongoing api call', async () => {
       await setupForm();
       await userEvent.click(button);
       await userEvent.click(button);
       await waitFor(() => {
         expect(counter).toBe(1);
-      })
-    })
+      });
+    });
     it('displays spinner after clicking the submit', async () => {
       await setupForm();
-      expect(screen.queryByRole("status")).not.toBeInTheDocument();
+      expect(screen.queryByRole('status')).not.toBeInTheDocument();
       await userEvent.click(button);
-      expect(screen.queryByRole("status")).toBeInTheDocument();
-    })
+      expect(screen.queryByRole('status')).toBeInTheDocument();
+    });
     it('displays account activation notification after successful sign up request', async () => {
       await setupForm();
-      expect(screen.queryByText('Please check your e-mail to activate your account')).not.toBeInTheDocument();
+      expect(
+        screen.queryByText('Please check your e-mail to activate your account')
+      ).not.toBeInTheDocument();
       await userEvent.click(button);
-      const text = await screen.findByText('Please check your e-mail to activate your account')
+      const text = await screen.findByText(
+        'Please check your e-mail to activate your account'
+      );
       expect(text).toBeInTheDocument();
-    })
+    });
     it('hides sign up form after successful sign up request', async () => {
       await setupForm();
       const form = screen.getByTestId('form-sign-up');
       await userEvent.click(button);
-      await screen.findByText('Please check your e-mail to activate your account')
+      await screen.findByText(
+        'Please check your e-mail to activate your account'
+      );
       expect(form).not.toBeInTheDocument();
-    })
+    });
   });
   describe('Validation', () => {
-    it('displays Username is required message when username is null', async () => {
-      await setup();
-      const message = "Username is required";
-      expect(screen.queryByText(message)).not.toBeInTheDocument();
-      const usernameInput = screen.getByLabelText('Username');
-      await userEvent.click(usernameInput);
-      await userEvent.tab();
-      expect(screen.queryByText(message)).toBeInTheDocument();
-    })
-    it('displays length error when username is less than 4 characters', async () => {
-      await setup();
-      const message = "Username must be at least 4 characters long";
-      expect(screen.queryByText(message)).not.toBeInTheDocument();
-      const usernameInput = screen.getByLabelText('Username');
-      await userEvent.type(usernameInput, '123');
-      await userEvent.tab();
-      expect(screen.queryByText(message)).toBeInTheDocument();
-    })
-  })
+    it.each`
+      label         | inputValue              | message
+      ${'Username'} | ${'{space}{backspace}'} | ${'Username is required'}
+      ${'Username'} | ${'123'}                | ${'Username must be at least 4 characters long'}
+    `(
+      'displays $message when $label has the value "$inputValue"',
+      async ({ label, inputValue, message }) => {
+        await setup();
+        expect(screen.queryByText(message)).not.toBeInTheDocument();
+        const input = screen.getByLabelText(label);
+        await userEvent.type(input, inputValue);
+        await userEvent.tab();
+        expect(screen.queryByText(message)).toBeInTheDocument();
+      }
+    );
+  });
 });
--- src/app/sign-up/sign-up.component.spec.ts
+++ src/app/sign-up/sign-up.component.spec.ts
@@ -170,26 +170,24 @@ describe('SignUpComponent', () => {
     })
   })
   describe('Validation', () => {
-    it('displays Username is required message when username is null', () => {
-      const signUp = fixture.nativeElement as HTMLElement;
-      expect(signUp.querySelector('div[data-testid="username-validation"]')).toBeNull();
-      const usernameInput = signUp.querySelector('input[id="username"]') as HTMLInputElement;
-      usernameInput.dispatchEvent(new Event('focus'));
-      usernameInput.dispatchEvent(new Event('blur'));
-      fixture.detectChanges();
-      const validationElement = signUp.querySelector('div[data-testid="username-validation"]');
-      expect(validationElement?.textContent).toContain('Username is required');
-    })
-    it('displays length error when username is less than 4 characters', () => {
-      const signUp = fixture.nativeElement as HTMLElement;
-      expect(signUp.querySelector('div[data-testid="username-validation"]')).toBeNull();
-      const usernameInput = signUp.querySelector('input[id="username"]') as HTMLInputElement;
-      usernameInput.value = "123"
-      usernameInput.dispatchEvent(new Event('input'));
-      usernameInput.dispatchEvent(new Event('blur'));
-      fixture.detectChanges();
-      const validationElement = signUp.querySelector('div[data-testid="username-validation"]');
-      expect(validationElement?.textContent).toContain('Username must be at least 4 characters long');
+
+    const testCases = [
+      { field: 'username', value: '', error: 'Username is required'},
+      { field: 'username', value: '123', error: 'Username must be at least 4 characters long'},
+    ]
+
+    testCases.forEach(({field, value, error }) => {
+      it(`displays ${error} when ${field} has '${value}'`, () => {
+        const signUp = fixture.nativeElement as HTMLElement;
+        expect(signUp.querySelector(`div[data-testid="${field}-validation"]`)).toBeNull();
+        const input = signUp.querySelector(`input[id="${field}"]`) as HTMLInputElement;
+        input.value = value;
+        input.dispatchEvent(new Event('input'));
+        input.dispatchEvent(new Event('blur'));
+        fixture.detectChanges();
+        const validationElement = signUp.querySelector(`div[data-testid="${field}-validation"]`);
+        expect(validationElement?.textContent).toContain(error);
+      })
     })
   })
 });
