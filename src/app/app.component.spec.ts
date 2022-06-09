2ea54841979cb1a37ef3d89d8515a1e5b651d948
Basar Buyukkahraman <basarbk@yahoo.com>
2022-06-10 00:00:03 +0300

    0602-routing-tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,51 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { routes } from './router/app-router.module';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { HomeComponent } from './home/home.component';
+import { Router } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { SharedModule } from './shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent, SignUpComponent, HomeComponent
       ],
+      imports: [
+        RouterTestingModule.withRoutes(routes),
+        HttpClientTestingModule,
+        SharedModule,
+        ReactiveFormsModule
+      ]
     }).compileComponents();
   });
-  it('passes', () => {
-    expect(true).toBe(true);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    router = TestBed.inject(Router);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  
+  describe('Routing', () => {
+    it('displays homepage at /', async () => {
+      await router.navigate(['/']);
+      fixture.detectChanges();
+      const page = fixture.nativeElement.querySelector('[data-testid="home-page"]');
+      expect(page).toBeTruthy();
+    })
+    it('displays sign up page at /signup', async () => {
+      await router.navigate(['/signup']);
+      fixture.detectChanges();
+      const page = fixture.nativeElement.querySelector('[data-testid="sign-up-page"]');
+      expect(page).toBeTruthy();
+    })
   })
 });
--- src/app/router/app-router.module.ts
+++ src/app/router/app-router.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { SignUpComponent } from '../sign-up/sign-up.component';
 
-const routes: Routes = [
+export const routes: Routes = [
   {
     path: '', component: HomeComponent
   },
