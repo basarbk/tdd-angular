29798dddb8aab6b368c05896b80e72fab699362e
Basar Buyukkahraman <basarbk@yahoo.com>
2022-06-10 00:17:54 +0300

    0602-routing-tests-jest

---
+++ src/app/app.component.jest.ts
@@ -0,0 +1,29 @@
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { render, screen } from '@testing-library/angular';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { routes } from './router/app-router.module';
+import { SharedModule } from './shared/shared.module';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+const setup = async (path: string) => {
+  const { navigate } = await render(AppComponent, {
+    declarations: [HomeComponent, SignUpComponent],
+    imports: [HttpClientModule, SharedModule, ReactiveFormsModule],
+    routes: routes,
+  });
+  await navigate(path);
+}
+
+describe('Routing', () => {
+  it.each`
+    path         | pageId
+    ${'/'}       | ${'home-page'}
+    ${'/signup'} | ${'sign-up-page'}
+  `('displays $pageId when path is $path', async ({ path, pageId }) => {
+    await setup(path)
+    const page = screen.queryByTestId(pageId);
+    expect(page).toBeInTheDocument();
+  });
+});
